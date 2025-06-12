d1f552bb187934e8d66c2cf76221b67594f30067
Priyosmita <daspriyosmita2003@gmail.com>
2025-06-12 22:05:52 +0530

    header

---
+++ app/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <div className='bg-[#ffffff] w-full h-20 flex justify-between'>
+        <div className='text-gray-800 text-2xl font-bold flex items-center pl-10'>Education Management System</div>
+        <div className='text-black text-xl flex items-center pr-10'>Profile</div>
+      </div>
+    </>
+  )
+}
+
+export default Header
