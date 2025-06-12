0662c6da002a9a4713bdcab5f134c60d70243c47
Priyosmita <daspriyosmita2003@gmail.com>
2025-06-12 23:06:33 +0530

    removed firebase oauth

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,13 +1,34 @@
 import React from 'react'
 
 const Navbar = () => {
-  return (
-    <>
-    <div className='bg-gray-100 shadow-xl h-60 w-72 rounded-lg'>
+    return (
+        <>
+            <div className='bg-gray-100 shadow-xl h-fit w-72 rounded-lg flex flex-col gap-y-6'>
+                <div className='text-xl font-bold text-gray-800 p-6'>Navigation</div>
 
-    </div>
-    </>
-  )
+                <div>
+                    Students
+                </div>
+
+                <div>
+                    Teachers
+                </div>
+
+                <div>
+                    Fees
+                </div>
+
+                <div>
+                    Batch
+                </div>
+
+                <div>
+                    Reports
+                </div>
+
+            </div>
+        </>
+    )
 }
 
 export default Navbar
